44eb80e6ec6d5b3e1d839e25aea15e20e0f44404
norama <norama.matema@gmail.com>
2017-05-25 13:33:24 +0200

    XPath syntax validation

--- src/Scraper.js
+++ src/Scraper.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import styles from './Scraper.css';
+import './Scraper.css';
+import './MessageBoxes.css'
+
+import XPathAnalyzer from 'xpath-analyzer';
 
 export default class Scraper extends React.PureComponent {
 	constructor(props) {
 		super(props);
 		
 		this.state = {
-			xpath: ''
+			xpath: '',
+			xpathError: null
 		};
 		
 		this.handleChange = this.handleChange.bind(this);
@@ -18,8 +22,12 @@ export default class Scraper extends React.PureComponent {
 			<form onSubmit={this.handleSubmit}>
 				<label>
 				XPath:
-				<input type="text" name="xpath" value={this.state.xpath} onChange={this.handleChange} />
+				<input type="text" name="xpath" value={this.state.xpath} onChange={this.handleChange} className={!this.state.xpathError ? 'validInput' : 'invalidInput'} />
 				</label>
+				{this.state.xpathError ?
+				(<div className='error'>
+					{this.state.xpathError.message}
+				</div>) : null}
 				<input type="submit" value="Submit" />
 			</form>
 		);
@@ -27,14 +35,39 @@ export default class Scraper extends React.PureComponent {
 	
 	handleChange(event) {
 		const xpath = event.target.value;
-		console.log('xpath', xpath);
 		this.setState({
-			xpath
+			xpath,
+			xpathError: null
 		});
 	}
 	
-	handleSubmit() {
-		alert(this.state.xpath);
+	handleSubmit(event) {
+		const xpathError = validateXPath(this.state.xpath);
+		if (xpathError) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
+		this.setState({
+			xpathError
+		}, () => {
+			if (!this.state.xpathError) {
+				// TODO
+				console.log('xpath', this.state.xpath);
+			} 
+		});
 	}
 };
+
+function validateXPath(xpath) {
+	try {
+		const analyzer = new XPathAnalyzer(xpath);
+		analyzer.parse();
+		
+		return null;
+	} catch (ex) {
+		console.error('XPath syntax check failed.', ex);
+		
+		return ex;
+	}
+}
 
